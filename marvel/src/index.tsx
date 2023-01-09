3c8b9d1082ec3ca1b4c02a7b7386bfd616bacda7
Chagas-Thiago <email@example.com>
2023-01-10 00:23:20 +0100

    feature:add api marvel

--- marvel/src/index.tsx
+++ marvel/src/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import App from './App'
 import './index.css'
 
+const transformMinutesToMilliseconds = (minutes: number) => {
+  return 1000 * 60 * minutes
+}
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+      queries: {
+          cacheTime: transformMinutesToMilliseconds(20),
+          staleTime: transformMinutesToMilliseconds(10),
+      }
+  }
+})
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+     <QueryClientProvider client={queryClient}>
+      <App />
+      </QueryClientProvider>
     </BrowserRouter >,
   document.getElementById("root")
 )
