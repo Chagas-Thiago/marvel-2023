19be9dcccd407395999b80e9a00aedb91256edd8
Chagas-Thiago <email@example.com>
2023-01-12 00:19:01 +0100

    features: refacto

--- marvel/src/styles/headerPagesSection.styles.ts
+++ marvel/src/styles/headerPagesSection.styles.ts
@@ -1,16 +1,39 @@
 import { makeStyles } from "@material-ui/core"
 
-const useStylesHeader = makeStyles((theme)=>({
+const useStylesHeader = makeStyles((theme) => ({
     root: {
         '& > *': {
-          margin: theme.spacing(1),
-          width: '25ch',
+            margin: theme.spacing(1),
+            width: '25ch',
         },
-      },
+    },
+    containerHeader: {
+        backgroundColor: "#3B3B3B",
+        justifyContent: "space-around"
+    },
     container: {
         height: 100,
         width: "100%"
-}
+    },
+    projectMarvelText: {
+        color: "white",
+        textAlign: "center"
+    },
+    containerInputeAndaBottom: {
+        height: 45,
+        alignItems: "center"
+    },
+    input: {
+        height: 35,
+        borderRadius: 5,
+        width: "50%",
+        backgroundColor: "#ffff"
+    },
+    button: {
+        marginLeft: "1%",
+        height: 35,
+        backgroundColor: "#EE1C25"
+    }
 }))
 
 export default useStylesHeader
